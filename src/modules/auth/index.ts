f01af4bff85588e90fd9e087e9f600f23c6587f5
Manish Singh <100526847+manish5476@users.noreply.github.com>
2026-06-17 16:10:23 +0530

    Update index.ts

--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -2,3 +2,7 @@ export * from './auth.routes';
 export * from './auth.controller';
 export * from './auth.service';
 export * from './auth.model';
+export * from './auth.validation';// export * from './auth.routes';
+// export * from './auth.controller';
+// export * from './auth.service';
+// export * from './auth.model';
